c2fd1f90e9e37e70ed9ea439e5e295abf7da3dcd
TFinch <travisfinch01@gmail.com>
2019-02-26 21:48:04 -0500

    Check for PickeTickets in HotFolder

--- FileMover/RunManualForm.Designer.cs
+++ FileMover/RunManualForm.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Name_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Name_Button
+            // 
+            this.Name_Button.Location = new System.Drawing.Point(338, 357);
+            this.Name_Button.Name = "Name_Button";
+            this.Name_Button.Size = new System.Drawing.Size(75, 23);
+            this.Name_Button.TabIndex = 0;
+            this.Name_Button.Text = "Run";
+            this.Name_Button.UseVisualStyleBackColor = true;
+            this.Name_Button.Click += new System.EventHandler(this.Name_Button_Click);
+            // 
+            // RunManualForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Name_Button);
+            this.Name = "RunManualForm";
             this.Text = "RunManualForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Name_Button;
     }
 }
--- FileMover/RunManualForm.cs
+++ FileMover/RunManualForm.cs
@@ -7,14 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FileMover.Constants.FileLocations;
+using FileMover.Constants.FileLocations;
+using FileMover.Modules;
+using System;
+using System.IO;
 
 namespace FileMover
 {
     public partial class RunManualForm : Form
     {
+        //Read the live full file name
+        public string LiveFileName = new Modules.FileName().FullOrderName;
+        public string LivePrimeName = new Modules.FileName().FullPrimeName;
+        public string whMI = new Constants.WarehouseLocations.WhLocations().MI;
+        public string whIL = new Constants.WarehouseLocations.WhLocations().IL;
+        public string LiveOrderNumber = new Modules.FileName().FullOrderName.Split().Last();
+
         public RunManualForm()
         {
             InitializeComponent();
+        }
+
+        private void Name_Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Check for MI PickTickets
+                if (LiveFileName.Contains(whMI) || (LiveOrderNumber.Contains(LiveFileName)))
+                {
+                    File.Move(Constants.FileLocations.Folders.HotFolder, Constants.FileLocations.Folders.path01A);
+                }else
+                {
+                    Console.WriteLine("The process failed: {0}");
+                }
+
+                //Check for IL PickTickets
+                if (LiveFileName.Contains(whIL) || (LiveOrderNumber.Contains(LiveFileName)))
+                {
+                    File.Move(Constants.FileLocations.Folders.HotFolder, Constants.FileLocations.Folders.path02A);
+                }else
+                {
+                    Console.WriteLine("The process failed: {0}");
+                }
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("The process failed: {0}", error.ToString());
+            }
+
         }
     }
 }
